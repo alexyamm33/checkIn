9373a960e595b9b67b622649a30e13c267548f2f
Alex Yeh <ayeh0330@gmaill.com>
2022-10-01 15:55:13 -0400

    Initial Commit

---
+++ checkIn/checkInApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkInApp.swift
+//  checkIn
+//
+//  Created by Alex Yeh on 2022-10-01.
+//
+
+import SwiftUI
+
+@main
+struct checkInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
